a7615be027207739f88e5aaf10c896132bd35733
Joan Manuel González Santacruz <joan.gs@192.168.1.108>
2025-10-06 12:40:51 -0500

    Initiate Go project

---
+++ cmd/api/handlers.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+}
+
+func (app *application) Login(w http.ResponseWriter, r *http.Request) {
+	type credentials struct {
+		UserName string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	var creds credentials
+	var payload jsonResponse
+
+	err := app.readJSON(w, r, &creds)
+	if err != nil {
+		app.errorLog.Println(err)
+
+		payload.Error = true
+		payload.Message = "Invalid JSON supplied or JSON missing entirely"
+
+		_ = app.writeJSON(w, http.StatusBadRequest, payload)
+
+		return
+	}
+
+	app.infoLog.Println(creds.UserName, creds.Password)
+
+	payload.Error = false
+	payload.Message = "Signed in"
+
+	err = app.writeJSON(w, http.StatusOK, payload)
+	if err != nil {
+		app.errorLog.Println(err)
+	}
+}
